fcf16b67566c99a84906769d3809d646356df047
George Nikolaidis <nove@novesoftware.net>
2025-08-27 19:38:21 +0300

    Add some more documentation

--- contracts/Pets.sol
+++ contracts/Pets.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract Pets {
+
     /// A value object describing the pet's status in the contract.
     struct PetStatus {
         bool isActive;
@@ -9,6 +10,7 @@ contract Pets {
         uint modifiedAt;
     }
 
+    /// Main domain entity for this contract
     struct Pet {
         string name;
         string kind;
